1c23710e16ca160445dade7defba35b3e6e55f30
James100000000000 <91449487+James100000000000@users.noreply.github.com>
2022-03-24 20:58:45 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,45 @@
-from this import d
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 
-dataset = pd.read_csv('Data.csv')
+
+dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:,:-1].values
-Y = dataset.iloc[:,-1].values
+y = dataset.iloc[:,-1].values
 
-from sklearn.model_selection
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:,1:3])
+
+print(X)
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+print("\n------------------------------\n")
+
+print(X)
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print("\n------------------------------\n",y,"\n------------------------------\n")
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state= 1)
+
+print(X_test,"\n\n------------------------------\n")
+print(y_train,"\n\n------------------------------\n")
+print(y_test,"\n\n------------------------------\n")
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+
+print(X_train,"\n\n------------------------------\n")
+print(X_test,"\n\n------------------------------\n")
